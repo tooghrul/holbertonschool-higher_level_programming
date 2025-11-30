fd4f8dd58593c43ab4a21716ef694c7c51bded97
tooghrul <toghrulallahverdiev@gmail.com>
2025-11-30 05:50:58 +0400

    printcomb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for i in range(9):
     for j in range(i+1, 10):
         if i == 8:
-            print("{}{}".format(i,j))
+            print("{}{}".format(i, j))
         else:
-            print("{}{}".format(i,j), end=", ")
-        
+            print("{}{}".format(i, j), end=", ")        
