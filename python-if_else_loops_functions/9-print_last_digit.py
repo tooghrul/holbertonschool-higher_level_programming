d7b6ec6575b7f263a5a03da9fd33e2ab4456942d
tooghrul <toghrulallahverdiev@gmail.com>
2025-11-30 06:44:15 +0400

    uppercase

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if isalnum(number) == True:
+    if isalpha(number) == True:
         return
     digit = abs(number) % 10
     print(digit, end="")
