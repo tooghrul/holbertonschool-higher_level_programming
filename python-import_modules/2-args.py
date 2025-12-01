85778a86c1e41d354e8587c996ee0568cbc7224f
T <t@abc.com>
2025-12-01 13:05:12 +0000

    2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,8 +6,8 @@ def main():
     if len(argv) == 0:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv)) if len(argv) != 1 else "1 argument:")
         for i in range(len(argv)):
-            print("{}: {}".format(i, argv[i]))
+            print("{}: {}".format(i+1, argv[i]))
 if __name__ == "__main__":
     main()
