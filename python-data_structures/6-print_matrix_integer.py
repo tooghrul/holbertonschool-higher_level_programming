9965eeed2fb5cc8e9ddee262ffca9769d9daa158
T <t@abc.com>
2025-12-04 09:52:51 +0000

    Done.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             if i != len(row)-1:
                 print("{:d}".format(row[i]), end=" ")
             else:                
-                print("{:d}".format(row[i]), end="")
+                print("{:d}".format(row[i]), end="\n")
