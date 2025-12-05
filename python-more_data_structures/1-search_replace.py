3d332963fa60a93b232cdbb034087a050d13b4ce
T <t@abc.com>
2025-12-05 09:32:18 +0000

    Done.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list.replace(search, replace)
+    for i in range(len( my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
     return my_list
-print (search_replace([1,2,3,4,5,6,6,7,7,7,7,7], 7, 13))
