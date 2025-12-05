5687992e3e966e1d1df9d5f37f0be84683be2af4
T <t@abc.com>
2025-12-05 10:15:48 +0000

    Done.

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = dict(a_dictionary)
     values = list(map(lambda x: x*2, a_dictionary.values()))
     for key, i in zip(a_dictionary.keys(), list(range(len(values)))):
-        a_dictionary[key] = values[i]
-    return a_dictionary
+        new_dict[key] = values[i]
+    return new_dict
