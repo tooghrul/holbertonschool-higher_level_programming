020dea5d8609e556d7f6c65028bb2eef2b9ac1da
T <t@abc.com>
2025-12-05 10:20:49 +0000

    Done.

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_key, max_num = None, None
+    for key, v in a_dictionary.items():
+        if v > max:
+            max_key = key
+            max_num  v
+    return max_key
