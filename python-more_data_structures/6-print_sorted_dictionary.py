96eb734aaaf445141054d14a7ad68a23367678ad
T <t@abc.com>
2025-12-05 09:57:00 +0000

    Done.

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_dictionary.sort()
+    a_dictionary = sorted(a_dictionary)
     for key, v in a_dictionary.items():
         print("{}: {}".format(key, v))
