6bbea136e0fe3ec1631065dee993d62d401b0a5b
T <t@abc.com>
2025-12-05 10:47:36 +0000

    Done.

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    i = 0
+    cem = 0
+    while i <= len(roman_string):
+        if roman[roman_string[i]] < roman[roman_string[i+1]]:
+            cem += roman[roman_string[i]] - roman[roman_string[i+1]]
+            i += 2
+        else:
+            cem += roman[roman_string[i]]
+            i+=1
+    return cem
