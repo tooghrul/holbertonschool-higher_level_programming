45942ff632d0a96e11cba7cb3d7a578cc5277511
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-06 14:02:32 +0400

    Roman intger

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str:
+    if type(roman_string) is not str:
         return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     i = 0
@@ -17,5 +17,5 @@ def roman_to_int(roman_string):
                 cem += roman[roman_string[i]]
         else:
             cem += roman[roman_string[i]]
-        i+=1
+        i += 1
     return cem
