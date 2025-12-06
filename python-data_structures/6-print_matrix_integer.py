1b34737369202d9f84a969cce10d68e8f96b68e0
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-06 14:15:26 +0400

    Print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is [[]]:
-        print(" ")
+        print()
+        return
     for row in matrix:
         for i in range(len(row)):
             if i != len(row)-1:
