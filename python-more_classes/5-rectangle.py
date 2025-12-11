bcb86546f9f43b2f777bafb25ad5c577b60d829c
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-11 12:15:01 +0400

    Done.

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This file creates a rectangle class"""
+
+
+class Rectangle:
+    """Represents a rectangle with width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width != 0 and self.__height != 0:
+            return (self.__width + self.__height) * 2
+        else:
+            return 0
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        x = ""
+        for i in range(self.__height):
+            if i == self.__height - 1:
+                x += self.__width*"#"
+            else:
+                x += self.__width*"#" + "\n"
+        return x
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        return "Bye rectangle..."
