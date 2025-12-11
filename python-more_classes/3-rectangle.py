c981b209f3575616f9f0b804c5b6422caa39d5fa
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-11 12:01:58 +0400

    Done.

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,6 +47,8 @@ class Rectangle:
         else:
             return 0
     def __str__(self):
+        if self.__height != 0 and self.__width != 0:
+            return ""
         x=""
         for i in range(self.__height):
             if i == self.__height - 1:
