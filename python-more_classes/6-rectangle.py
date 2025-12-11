46a1fad599068d7c94ea5993bbdd748ef1b36669
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-11 12:26:47 +0400

    Done.

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """Represents a rectangle with width and height."""
 
     number_of_instances = 0
+    
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with width and height."""
         self.width = width
