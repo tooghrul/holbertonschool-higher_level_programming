d237974dfda982b27053a6a91e11ec44291ff76f
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-11 17:58:11 +0400

    Done.

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -69,3 +69,16 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+    
+    @staticmethod
+    def bigger_of_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area >  rect_2.area:
+            return rect_1.area
+        elif rect_1.area == rect_2.area:
+            return rect_1.area
+        else:
+            return rect_2.area
