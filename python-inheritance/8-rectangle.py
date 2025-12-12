09943be85ee559d791a69991e8b3741f3b30a30b
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-12 21:22:20 +0400

    Done.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -14,6 +14,7 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """This is a Rectangle class which inherits from BaseGeometry class"""
     def __init__(self, width, height):
         super().__init__
         try:
