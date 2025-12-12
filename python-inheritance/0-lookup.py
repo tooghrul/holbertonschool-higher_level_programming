d73358f936aac04eaa4e72b19f26e17f9b9cb874
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-12 20:29:14 +0400

    Done.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-
+"""This module returns a list objects that demonstrates class methods etc."""
 def lookup(obj):
+    """This method returns the list"""
     return dir(obj)
+
+if __name__ == "__main__":
+    lookup(obj)
