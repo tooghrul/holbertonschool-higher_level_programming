716c39b51cec82545a09dc253c4199be2a5f0744
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-12 20:53:14 +0400

    Done.

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,10 +2,10 @@
 """This func. checks if the object is the instance of the class"""
 
 
-def is_same_class(obj, a_class):
-    """It uses type()"""
+def is_kind_of_class(obj, a_class):
+    """It uses isinstance()"""
     return isinstance(obj, a_class) is True
 
 
 if __name__ == "__main__":
-    is_same_class()
+    is_kind_o_class()
