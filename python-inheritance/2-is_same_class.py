2e747a14e34fa2bd1eef4e325d4d0ea14bdc6521
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-12 20:44:46 +0400

    Done.

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This func. checks if the object is the instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """It uses isinstance()"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    is_same_class()
