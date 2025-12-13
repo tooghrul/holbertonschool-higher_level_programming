dac17b9fb383ed1b22d53cbb8a1f133f840990b5
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-14 03:24:48 +0400

    IO/PascalTriangle updated

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Pascal triangle"""
 
+
 def pascal_triangle(n):
     """It is a pascal trianle function"""
     if n <= 0:
@@ -9,9 +10,9 @@ def pascal_triangle(n):
     if n == 1:
         return [[1]]
 
-    pascal = [[1], [1,1]]
+    pascal = [[1], [1, 1]]
     prev_pascal = pascal[1]
-    for step in range(3,n+1):
+    for step in range(3, n+1):
         base = [0 if i != 0 and i != step - 1 else 1 for i in range(step)]
         for i in range(1, step-1):
             base[i] = prev_pascal[i] + prev_pascal[i-1]
