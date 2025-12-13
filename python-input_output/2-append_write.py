904d9dbc745682c2556efdd2a1fcaa4b06b6fc0b
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-13 16:32:57 +0400

    IO/2-append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This function appends the text that is passed through the function"""
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Using with command"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
