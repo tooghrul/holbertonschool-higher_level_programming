724085da0140b35340cb36f2ed4c3ab8526a472a
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-13 15:58:16 +0400

    changes in 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -21,7 +21,6 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height
         self.__width = width
-        super().__init__()
 
     def area(self):
         return self.__width * self.__height
@@ -35,7 +34,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
