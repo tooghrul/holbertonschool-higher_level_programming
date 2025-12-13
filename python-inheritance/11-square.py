a1f3e08bc11ee37c6db52166b25177ba94230610
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-13 15:30:40 +0400

    11-square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This class is an empty class which is named BaseGeometry"""
+
+
+class BaseGeometry:
+    """A kinda empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """This is a Rectangle class which inherits from BaseGeometry class"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("height", height)
+        self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """This is a Square class which inherits from Rectanle class"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        return self.__size ** 2 
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
