ba21bfb5d6d0cf52da18655cbcfb360048aab874
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-14 03:20:52 +0400

    PascalTriangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    if n == 1:
+        return [1]
+
+    pascal = [[1], [1,1]]
+    prev_pascal = pascal[1]
+    for step in range(3,n+1):
+        base = [0 if i != 0 and i != step - 1 else 1 for i in range(step)]
+        for i in range(1, step-1):
+            base[i] = prev_pascal[i] + prev_pascal[i-1]
+        pascal.append(base)
+        prev_pascal = base
+    return pascal
+
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
