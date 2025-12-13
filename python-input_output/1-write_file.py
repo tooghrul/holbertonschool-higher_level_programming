a8f4173d036478eb06947b74715d7a981ffddd9b
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-13 16:29:14 +0400

    IO/1-writefile

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function writes the text that is passed through the function"""
+
+
+def write_file(filename="", text=""):
+    """Using with command"""
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
