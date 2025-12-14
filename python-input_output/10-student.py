7e243824eb4ee6a4b186de4e386e5e2d706c9db9
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-14 06:07:50 +0400

    IO/10student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,12 +4,16 @@
 
 class Student:
     """Student class"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=[]):
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the instance.
+        If attrs is a list of strings, only include those attributes.
+        """
         if attrs is None:
             return self.__dict__.copy()
         else:
@@ -17,4 +21,4 @@ class Student:
             for key, value in self.__dict__.items():
                 if key in attrs:
                     new_dict[key] = value
-        return new_dict
+            return new_dict
