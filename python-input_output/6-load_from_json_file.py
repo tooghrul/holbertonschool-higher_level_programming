f9d87e5dda760739a9dfd8656f3914dbd1e5e280
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-14 04:06:49 +0400

    IO/6loadformjson

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a function that writes an Object to a text file using a JSON repr."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """This function writes <my_obj> to <filename>"""
+    with open(filename, "r") as f:
+        obj = json.loads(filename)
+    return obj
