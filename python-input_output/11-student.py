28ced520245e6c985c1e28844589d0444701ebb9
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-14 06:04:45 +0400

    IO/11student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by first_name, last_name and so on"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the instance.
+        If attrs is a list of strings, only include those attributes.
+        """
+        if attrs is None:
+            return self.__dict__.copy()
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the instance with the values from json"""
+        for key, value in json.items():
+            setattr(self, key, value)
