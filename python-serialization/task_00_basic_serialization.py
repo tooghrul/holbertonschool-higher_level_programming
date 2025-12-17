843249d6d60c065a1b14a8ccf0cd7cd6654fd196
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-17 11:56:22 +0400

    serial/00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, mode="w", encoding="utf-8") as fn:
+        json.dump(data, fn, indent=4)
+
+def load_and_deserialize(filename):
+    with open(filename, mode="r", encoding="utf-8") as fn:
+        return json.load(filename)
