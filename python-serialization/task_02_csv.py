a39397d74d0289b3a4761ba7809afd6343c05807
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-17 12:47:49 +0400

    serial/02

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,11 +4,15 @@ import csv
 import json
 
 def convert_csv_to_json(csv_file):
-    context = csv.DictReader(csv_file)
     try:
+        with open(csv_file, newline="") as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+
         with open("data.json", "w") as f:
-            json.loads(f)
+            json.dump(data, f)
+
+        return True
     except Exception:
         return False
-    else:
-        return True
+
