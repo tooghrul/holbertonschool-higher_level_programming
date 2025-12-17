4e0e2014d63cd7569d5621a3e2ed6b3b241d922c
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-17 12:20:17 +0400

    serial/01

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import pickle
+import json
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"
+            Name: {self.name}
+            Age: {self.age}
+            Is Student: {self.is_student}
+        ")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
