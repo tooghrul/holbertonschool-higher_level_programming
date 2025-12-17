54c8bf9cb4d331e95e318de79e3d5c0c8037beda
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-17 14:36:15 +0400

    sql/9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Creating the first table --
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+
+INSERT INTO second_table(1, 'John', 10);
+INSERT INTO second_table(2, 'Alex', 3);
+INSERT INTO second_table(3, 'Bob', 14);
+INSERT INTO second_table(4, 'George', 8);
