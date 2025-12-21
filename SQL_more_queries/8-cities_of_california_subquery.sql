32c9cb231b200b9a7ccc8c0b0b2e8906deeabbfc
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:15:25 +0400

    sqlmore/8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,11 @@
--- Cities of California --
-SELECT *
+-- Lists all cities of California found in hbtn_0d_usa
+-- Uses a subquery to avoid the JOIN keyword
+-- Results are sorted in ascending order by cities.id
+SELECT id, name
 FROM cities
 WHERE state_id = (
-    SELECT id
-    FROM states
+    SELECT id 
+    FROM states 
     WHERE name = 'California'
 )
 ORDER BY id ASC;
