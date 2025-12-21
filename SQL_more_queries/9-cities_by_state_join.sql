0f37ec27bfc8f89c50ae4a7b542691925c55efda
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:12:25 +0400

    sqlmore/9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,7 @@
--- Cities by state --
-SELECT cities.id, cities.name, 
-       (SELECT name FROM states WHERE states.id = cities.state_id) AS state_name
+-- Lists all cities contained in the database hbtn_0d_usa
+-- Each record displays: cities.id - cities.name - states.name
+-- Results are sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
 FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
