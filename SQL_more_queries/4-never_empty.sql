7bfdc528423168eb77983a84fe36f7caa0d7d55a
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:21:42 +0400

    sqlmore/4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,8 @@
--- Never empty --
-
-CREATE TABLE id_not_null (id INT DEFAULT 1, name VARCHAR(256)) 
-IF NOT EXISTS id_not_null;
+-- Creates the table id_not_null on your MySQL server
+-- id: INT with the default value 1
+-- name: VARCHAR(256)
+-- If the table already exists, the script will not fail
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
