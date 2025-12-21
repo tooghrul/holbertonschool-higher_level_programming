6aa05452edaed75b96fe5246e5455560baf35d32
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:13:18 +0400

    sqlmore/10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- Genre id --
-SELECT title, genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY title ASC, genre_id ASC;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
