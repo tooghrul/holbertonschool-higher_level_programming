c56dd2b4837757fb0f44f7b2e21b4dd087219684
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:20:00 +0400

    sql/15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,7 @@
--- Score and count --
-
-SELECT COUNT(score) AS number, score
+-- Lists the number of records with the same score in the table second_table
+-- Displays: score, number (count of records with that score)
+-- Results are sorted by the number of records in descending order
+SELECT score, COUNT(*) AS number
 FROM second_table
+GROUP BY score
 ORDER BY number DESC;
