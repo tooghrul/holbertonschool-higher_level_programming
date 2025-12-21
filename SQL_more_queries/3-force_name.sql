0bb4b6d7eabd5fb52c7c03fa1e10dfb830f23314
tooghrul <toghrulallahverdiev@gmail.com>
2025-12-21 04:21:08 +0400

    sqlmore/3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,8 @@
--- Force name --
-
-CREATE TABLE force_name(id INT, name VARCHAR(256) NOT NULL)
-IF NOT EXISTS force_name;
+-- Creates the table force_name on your MySQL server
+-- id: INT
+-- name: VARCHAR(256), cannot be NULL
+-- If the table already exists, the script will not fail
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
